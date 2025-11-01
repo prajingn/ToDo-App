67985813ce4c2e4f561e40fca7b7725da17a0c48
prajingn <prajin376@gmail.com>
2025-11-01 20:10:09 +0530

    Add pending and complete views, add edit page
    Add edit page with place holder text, added task edit buttons
    restructured table column width to stay consistant.

--- todo/models.py
+++ todo/models.py
@@ -1,17 +1,10 @@
 from datetime import date
 from todo import db
 
-# class User(db.Model):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     name = db.Column(db.String(length=30), nullable=False, unique=True)
-#     password = db.Column(db.String(length=60), nullable=False)
-#     tasks = db.relationship("Task", backref="owned_user", lazy=True)
-
 class Task(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    name = db.Column(db.String(length=30), nullable=False)
     details = db.Column(db.String(length=150), nullable=False)
     due = db.Column(db.Date())
-    priority = db.Column(db.Integer(), default=0 )
+    priority = db.Column(db.String(length=10), default=0 )
     is_complete = db.Column(db.Boolean(), default=False)
-    # owner = db.Column(db.Integer(), db.ForeignKey("user.id"))
--- todo/routes.py
+++ todo/routes.py
@@ -27,3 +27,33 @@ def clear_all():
     db.drop_all()
     db.create_all()
     return redirect(url_for("pending"))
+
+@app.route("/task_options", methods=["GET", "POST"])
+def task_options():
+    task_id = int(request.form["id"])
+    t = Task.query.get(task_id)
+
+    if request.form["btn"] == "complete":
+        t.is_complete = True
+        db.session.commit()
+
+    elif request.form["btn"] == "edit":
+        print("hello")
+        return render_template("edit_task.html", curr_date=curr_date, date=date, task_id=task_id, name=t.name, details=t.details)
+
+    elif request.form["btn"] == "delete":
+        db.session.delete(t)
+        db.session.commit()
+
+    return redirect(url_for("pending"))
+
+@app.route("/edit_task/<int:id>", methods=["POST"])
+def edit_task(id):
+    t = Task.query.get(id)
+    t.name=request.form["name"]
+    t.details=request.form["details"]
+    t.priority=request.form["priority"]
+    t.due = datetime.strptime(request.form["due"], "%Y-%m-%d").date()
+
+    db.session.commit()
+    return redirect(url_for("pending"))
